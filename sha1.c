7f23e33e6f9d71c594e580d00219c785e92d51c2
Jasin Bushnaief <jasin@iki.fi>
2016-08-17 09:33:58 +0300

    Use stdint.h instead of custom typedefs

--- sha1.c
+++ sha1.c
@@ -4,19 +4,19 @@
 
 #define ALIGN(x,a) (((x)+(a)-1ULL)&~((a)-1ULL))
 
-static UINT32 rotl32(UINT32 x, int b)
+static uint32_t rotl32(uint32_t x, int b)
 {
     return (x << b) | (x >> (32-b));
 }
 
 // switch endianness
-static UINT32 get32 (const void* p)
+static uint32_t get32 (const void* p)
 {
-    const UINT8 *x = (const UINT8*)p;
+    const uint8_t *x = (const uint8_t*)p;
     return (x[0] << 24) | (x[1] << 16) | (x[2] << 8) | x[3];
 }
 
-static UINT32 f (int t, UINT32 b, UINT32 c, UINT32 d)
+static uint32_t f (int t, uint32_t b, uint32_t c, uint32_t d)
 {
     assert(0 <= t && t < 80);
 
@@ -31,9 +31,9 @@ static UINT32 f (int t, UINT32 b, UINT32 c, UINT32 d)
 }
 
 
-static void processBlock (const UINT32* block, UINT32* h)
+static void processBlock (const uint32_t* block, uint32_t* h)
 {
-    static const UINT32 k[4] =
+    static const uint32_t k[4] =
     {
         0x5A827999,
         0x6ED9EBA1,
@@ -41,12 +41,12 @@ static void processBlock (const UINT32* block, UINT32* h)
         0xCA62C1D6
     };
 
-    UINT32 w[80];
-    UINT32 a = h[0];
-    UINT32 b = h[1];
-    UINT32 c = h[2];
-    UINT32 d = h[3];
-    UINT32 e = h[4];
+    uint32_t w[80];
+    uint32_t a = h[0];
+    uint32_t b = h[1];
+    uint32_t c = h[2];
+    uint32_t d = h[3];
+    uint32_t e = h[4];
     int t;
 
     for (t = 0; t < 16; t++)
@@ -59,7 +59,7 @@ static void processBlock (const UINT32* block, UINT32* h)
     for (t = 0; t < 80; t++)
     {
         int s = t & 0xf;
-        UINT32 temp;
+        uint32_t temp;
         if (t >= 16)
             w[s] = rotl32(w[(s + 13) & 0xf] ^ w[(s + 8) & 0xf] ^ w[(s + 2) & 0xf] ^ w[s], 1);
 
@@ -67,7 +67,7 @@ static void processBlock (const UINT32* block, UINT32* h)
 
          e = d; d = c; c = rotl32(b, 30); b = a; a = temp;
         /*
-        UINT32 temp = rotl32(a, 5) + f(t, b,c,d) + e + w[t] + k[t/20];
+        uint32_t temp = rotl32(a, 5) + f(t, b,c,d) + e + w[t] + k[t/20];
         e = d;
         d = c;
         c = rotl32(b, 30);
@@ -83,14 +83,14 @@ static void processBlock (const UINT32* block, UINT32* h)
     h[4] += e;
 }
 
-static int padMsg (const void* msg, UINT64 bytes, UINT8* lastBlock)
+static int padMsg (const void* msg, uint64_t bytes, uint8_t* lastBlock)
 {
     int msgBytesInLast   = (int)(bytes & 63);
     int extraBlocks      = (msgBytesInLast + 9) > 64 ? 2 : 1;
     int numZeroBytes     = extraBlocks * 64 - 9 - msgBytesInLast;
 
     // fill remaining msg bytes
-    const UINT8* msgLast = (UINT8*)msg + (bytes & ~63);
+    const uint8_t* msgLast = (uint8_t*)msg + (bytes & ~63);
     while (msgBytesInLast--)
         *lastBlock++ = *msgLast++;
 
@@ -102,22 +102,22 @@ static int padMsg (const void* msg, UINT64 bytes, UINT8* lastBlock)
 
     // original length in bits (!), switch endianness
     bytes *= 8;
-    *lastBlock++ = (UINT8)(bytes >> 56 & 0xff);
-    *lastBlock++ = (UINT8)(bytes >> 48 & 0xff);
-    *lastBlock++ = (UINT8)(bytes >> 40 & 0xff);
-    *lastBlock++ = (UINT8)(bytes >> 32 & 0xff);
-    *lastBlock++ = (UINT8)(bytes >> 24 & 0xff);
-    *lastBlock++ = (UINT8)(bytes >> 16 & 0xff);
-    *lastBlock++ = (UINT8)(bytes >> 8  & 0xff);
-    *lastBlock++ = (UINT8)(bytes >> 0  & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 56 & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 48 & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 40 & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 32 & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 24 & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 16 & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 8  & 0xff);
+    *lastBlock++ = (uint8_t)(bytes >> 0  & 0xff);
 
     return extraBlocks;
 }
 
-SHA1_Digest SHA1_get (const void* msg, UINT64 bytes)
+SHA1_Digest SHA1_get (const void* msg, uint64_t bytes)
 {
     SHA1_Digest digest;
-    UINT32 h[5] =
+    uint32_t h[5] =
     {
         0x67452301,
         0xefcdab89,
@@ -126,13 +126,13 @@ SHA1_Digest SHA1_get (const void* msg, UINT64 bytes)
         0xc3d2e1f0
     };
 
-    UINT64 totalBlocks = ALIGN(bytes + 9, 64) / 64; // including padding
-    const UINT32* block = (const UINT32*)msg;
-    UINT64 b;
+    uint64_t totalBlocks = ALIGN(bytes + 9, 64) / 64; // including padding
+    const uint32_t* block = (const uint32_t*)msg;
+    uint64_t b;
 
     // we could assume that msg is always required to
     // hold padding, but let's not
-    UINT8 lastBlocks[128];  // either one or two blocks
+    uint8_t lastBlocks[128];  // either one or two blocks
     int numLast = padMsg(msg, bytes, lastBlocks);
 
     for (b = 0; b < totalBlocks-numLast; b++)
@@ -143,13 +143,13 @@ SHA1_Digest SHA1_get (const void* msg, UINT64 bytes)
 
     // process last block
     for (b = 0; b < numLast; b++)
-        processBlock((const UINT32*)(lastBlocks + b*64), h);
+        processBlock((const uint32_t*)(lastBlocks + b*64), h);
 
-    *((UINT32*)&digest.digest[0])  = get32(&h[0]);
-    *((UINT32*)&digest.digest[4])  = get32(&h[1]);
-    *((UINT32*)&digest.digest[8])  = get32(&h[2]);
-    *((UINT32*)&digest.digest[12]) = get32(&h[3]);
-    *((UINT32*)&digest.digest[16]) = get32(&h[4]);
+    *((uint32_t*)&digest.digest[0])  = get32(&h[0]);
+    *((uint32_t*)&digest.digest[4])  = get32(&h[1]);
+    *((uint32_t*)&digest.digest[8])  = get32(&h[2]);
+    *((uint32_t*)&digest.digest[12]) = get32(&h[3]);
+    *((uint32_t*)&digest.digest[16]) = get32(&h[4]);
     return digest;
 }
 
@@ -169,7 +169,7 @@ SHA1_Digest SHA1_Digest_fromStr (const char* src)
 
         c0 = '0' <= c0 && c0 <= '9' ? c0 - '0' : ('a' <= c0 && c0 <= 'f' ? 0xa + c0 - 'a' : -1);
         c1 = '0' <= c1 && c1 <= '9' ? c1 - '0' : ('a' <= c1 && c1 <= 'f' ? 0xa + c1 - 'a' : -1);
-        d.digest[i] = (UINT8)((c0 << 4) | c1);
+        d.digest[i] = (uint8_t)((c0 << 4) | c1);
     }
 
     return d;
--- sha1.h
+++ sha1.h
@@ -1,15 +1,13 @@
 #pragma once
 
-typedef unsigned char       UINT8;
-typedef unsigned int        UINT32;
-typedef unsigned long long  UINT64;
+#include <stdint.h>
 
 // struct so we can return it (it's 5x32bit)
 typedef struct _SHA1_Digest
 {
-    UINT8 digest[20];
+    uint8_t digest[20];
 } SHA1_Digest;
 
 SHA1_Digest SHA1_Digest_fromStr (const char* src);
 void        SHA1_Digest_toStr   (const SHA1_Digest* digest, char* dst);
-SHA1_Digest SHA1_get (const void* msg, UINT64 bytes);
+SHA1_Digest SHA1_get (const void* msg, uint64_t bytes);
